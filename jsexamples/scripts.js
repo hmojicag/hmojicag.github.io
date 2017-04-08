506ba171e446d5a80e1fe886c75e2333938935ca
hmojicacd <v-hmojica@ean.com>
2017-04-08 10:37:27 -0500

    JS Example

---
+++ jsexamples/scripts.js
@@ -0,0 +1,6 @@
+function msg() {
+    var val1 = Number(document.getElementById("textbox1").value);
+    var val2 = Number(document.getElementById("textbox2").value);
+    var sum = val1 + val2;
+    alert(sum);
+}
