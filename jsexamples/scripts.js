066863ffb03ac2fb7e5177e08fee52ec75ba2fe3
hmojicacd <v-hmojica@ean.com>
2017-05-12 16:25:54 -0500

    Al final comprendi esta mmda de Jekyll

--- jsexamples/scripts.js
+++
@@ -1,6 +0,0 @@
-function msg() {
-    var val1 = Number(document.getElementById("textbox1").value);
-    var val2 = Number(document.getElementById("textbox2").value);
-    var sum = val1 + val2;
-    alert(sum);
-}
